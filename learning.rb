26b9f0034ab2192912f569ac751837c2562fc242
boogity <harumph505@gmail.com>
2015-06-09 18:51:34 -0500

    added learning ruby program

--- learning.rb
+++ learning.rb
@@ -1,12 +1,51 @@
-class Greeter
-	def initialize(name)
-		name = @name
+class MegaGreeter
+	attr_accessor :names
+
+	#Create the object
+	def initialize(names = "World")
+	  @names = names
 	end
-	def say_hi(name) 
-		puts "Hello, #{@name}."
+	def say_hi 
+	  if @names.nil?
+	    puts "..."
+	  elsif @names.respond_to?("each")
+	    #@names is a list of some kind, iterate!
+	  	@names.each do |name|
+		  puts "Hello #{name}!"
+	  end
+	  else
+	    puts "Hello #{@names}!"
+	  end
 	end
-	def say_bye(name)
-		ptus "Goodbye, #{@name}."
+	def say_bye
+		if @names.nil?
+			puts "..."
+		elsif @names.respond_to?("join")
+		 #join the list of elements with a comma
+			puts "Goodbye #{@names.join(", ")}."
+		else
+			puts "Goodbye #{@names}."
+		end
 	end
 end
 
+if __FILE__ == $0
+	mg = MegaGreeter.new
+	mg.say_hi
+	mg.say_bye
+
+	#change name to be "Zeke"
+	mg.names = "Zeke"
+	mg.say_hi
+	mg.say_bye
+
+	#change the name to an array of names
+	mg.names = ["Albert", "Brenda", "Charles", "Dave", "Engelbert"]
+	mg.say_hi
+	mg.say_bye
+
+	#change to nil
+	mg.names = nil
+	mg.say_hi
+	mg.say_bye
+end
